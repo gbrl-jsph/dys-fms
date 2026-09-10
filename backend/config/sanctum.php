d050cb900c3245f881c2fc444aacf092134b30e8
Gabriel Joseph Miranda <akosigabriel50@gmail.com>
2026-09-10 21:44:45 +0800

    fix(auth): enable Sanctum web session guard

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -6,7 +6,7 @@ return [
         'localhost,localhost:3000,localhost:8000,localhost:8080,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
-    'guard' => [],
+    'guard' => ['web'],
     'expiration' => null,
     'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
     'middleware' => [
